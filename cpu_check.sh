85e9bf6708bf5faec6df3b745eeec31541f32fd4
littlepup5265 <78381900+littlepup5265@users.noreply.github.com>
2021-02-23 11:03:23 -0500

    Add files via upload

---
+++ cpu_check.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+for options in $(echo $@)
+
+do
+  SERVERLIST=$options
+done
+
+if [ ! -s "$SERVERLIST" ]
+then
+  echo "ERROR: $SERVERLIST does not exist"
+  exit 1
+fi
+
+
+
+for i in $(cat $SERVERLIST)
+do
+		echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+		echo $i
+		    rosh -l root -n $i -t 'cat /proc/cpuinfo | grep processor | wc -l'
+done			
